09e1703b10ba9d972bc3805d7da9f515b7e58e43
Osmar Junior <oj13black@hotmail.com>
2025-10-27 12:13:34 -0300

    01 - Merge de Strings

---
+++ 75Q/01-mergeStrings/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+class Solution
+{
+
+    public static void Main(string[] args)
+    {
+        string word1 = Console.ReadLine().Trim();
+        string word2 = Console.ReadLine().Trim();
+
+        string result = MergeAlternately(word1, word2);
+
+        Console.WriteLine(result);
+    }
+    //Solucao 1
+    public static string MergeAlternately(string word1, string word2)
+    {
+        StringBuilder resultado = new StringBuilder();
+        int contador = Math.Max(word1.Length, word2.Length);
+        for (int i = 0; i < contador; i++)
+        {
+            if (i < word1.Length) 
+                resultado.Append(word1[i]);
+
+            if (i < word2.Length)
+                resultado.Append(word2[i]);
+        }
+
+        return resultado.ToString();
+    }
+
+    //SOLUCAO MAIS RAPIDA LEETCODE
+    // public static string MergeAlternately(string word1, string word2)
+    // {
+    //     int j = 0;
+    //     string word = "";
+    //     if (word1.Length > word2.Length)
+    //     {
+    //         for (int i = 0; i < word2.Length; i++)
+    //         {
+    //             word += word1[i];
+    //             word += word2[i];
+    //             j = i;
+    //         }
+    //         word += word1.Substring(j + 1);
+    //     }
+    //     else
+    //     {
+    //         for (int i = 0; i < word1.Length; i++)
+    //         {
+    //             word += word1[i];
+    //             word += word2[i];
+    //             j = i;
+    //         }
+    //         word += word2.Substring(j + 1);
+    //     }
+    //     return word;
+    // }
+
+    //RESULTADO - MESCLANDO COM O MAIS RAPIDO
+    // public static string MergeAlternately(string word1, string word2)
+    // {
+    //     StringBuilder resultado = new StringBuilder();
+    //     int contador = Math.Min(word1.Length, word2.Length);
+    //     int j = 0;
+    //     for (int i = 0; i < contador; i++)
+    //     {
+    //         if (i < word1.Length)
+    //             resultado.Append(word1[i]);
+
+    //         if (i < word2.Length)
+    //             resultado.Append(word2[i]);
+
+    //         j = i;
+    //     }
+    //     resultado.Append(word1.ToString().Substring(j + 1)).Append(word2.ToString().Substring(j + 1));
+        
+    //     return resultado.ToString();
+    // }
+}
